ae61613296115565dd1bb08678526a24cc5ced2d
Maroš Ľuba <marosinko397@gmail.com>
2022-10-18 10:31:08 +0200

    edit responsive deisgn

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,25 +4,21 @@
 @section('page-content')
     
 
-<section class="mt-16 relative h-[540px] flex items-center justify-center">
-    
-        <img class=" absolute top-0 left-0" src="{{asset('img/slider1.jpg')}}" alt="fotka">
-        <div class="max-w-[1500px] mx-auto flex justify-start w-[1500px]">
-
-            <div class=" z-10">
-    
-                <h3 class=" text-6xl font-bold font-serif">We can fix it broken <br>mobile glass</h3>
+<section class="mt-16 relative lg:h-[540px] lg:flex items-center justify-center">
+    <img class=" hidden md:block absolute top-0 left-0" src="{{asset('img/slider1.jpg')}}" alt="fotka">
+    <div class=" max-w-[1500px] mx-auto flex justify-start lg:w-[1500px] mt-24 lg:mt-0">
+            <div class="z-10 text-center lg:text-left">
+                <h3 class=" text-4xl lg:text-6xl font-bold font-serif ">We can fix it broken <br>mobile glass</h3>
                 <p class="text-gray-600 mt-10">m Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the<br> 
                      the industry's standard dummy </p>
-                <button class=" transition duration-300 shadow bg-blue-400 hover:bg-blue-600 focus:shadow-outline focus:outline-none border-2 border-transparent hover:border-blue-400 text-white font-bold py-2 px-4 mt-10 mr-2 " type="button">
-                    Kúpiť
+                <button class="uppercase transition duration-300 shadow bg-blue-400 hover:bg-blue-600 focus:shadow-outline focus:outline-none border-2 border-transparent hover:border-blue-400 text-white font-bold py-2 px-4 mt-10 mr-2 " type="button">
+                    Buy
                 </button>
-    
-                <button class=" transition duration-300 shadow bg-blue-600 hover:bg-blue-400 focus:shadow-outline focus:outline-none border-2 border-transparent hover:border-blue-600 text-white font-bold py-2 px-4 " type="button">
-                    Kontaktovať
+                <button class="uppercase transition duration-300 shadow bg-blue-600 hover:bg-blue-400 focus:shadow-outline focus:outline-none border-2 border-transparent hover:border-blue-600 text-white font-bold py-2 px-4 " type="button">
+                    Contact us
                 </button>
             </div>
-        </div>
+    </div>
   
     
 </section>
@@ -30,34 +26,29 @@
 <section class="bg-white m-10">
     <div class="grid lg:grid-cols-3 md:grid-cols-2 max-w-[1300px] mx-auto p-9">
 
-    @include('include.oprava', [
-            'text' => 'We specialise in Phone repairs for Apple iPhones, iPad, iPod, Samsung, Galaxy, Sony, HTC, Nexus, Motorola, Blackberry & ...',
-            'image' => asset('img/oprava1.jpg'),
-            'title' => 'Smart Phone oprava'
-        ] )
-    
-    @include('include.oprava', [
-            'text' => 'If you are facing any problem with your Tablets / Ipads, Kindly pls go through the menttioned catagories ...',
-            'image' => asset('img/oprava2.jpg'),
-            'title' => 'Tablety a Ipady oprava'
-        ] )
+        @include('include.oprava', [
+                'text' => 'We specialise in Phone repairs for Apple iPhones, iPad, iPod, Samsung, Galaxy, Sony, HTC, Nexus, Motorola, Blackberry & ...',
+                'image' => asset('img/oprava1.jpg'),
+                'title' => 'Smart Phone repair'
+            ] )
+        
+        @include('include.oprava', [
+                'text' => 'If you are facing any problem with your Tablets / Ipads, Kindly pls go through the menttioned catagories ...',
+                'image' => asset('img/oprava2.jpg'),
+                'title' => 'Tablets & Ipad repair'
+            ] )
 
-    @include('include.oprava', [
-            'text' => 'We specialist in providing On-Site Computer Desktop Repair Service and Network Support for all sized business, On-Site Computer.',
-            'image' => asset('img/oprava3.jpg'),
-            'title' => 'Dosktopy a MAC oprava'
-        ] )
+        @include('include.oprava', [
+                'text' => 'We specialist in providing On-Site Computer Desktop Repair Service and Network Support for all sized business, On-Site Computer.',
+                'image' => asset('img/oprava3.jpg'),
+                'title' => 'Desktrop & MAC repair'
+            ] )
     </div>
-
-    
-
 </section>
 
 <section class="pt-16 bg-cover bg-no-repeat bg-center categories-bg bg-fixed">
-
-    <h2 class="text-center uppercase text-white text-5xl font-bold ">Kategórie</h2>
-
-    <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-3 max-w-[1300px] mx-auto py-16 ml-10">
+    <h2 class="text-center uppercase text-white text-5xl font-bold ">Categories</h2>
+    <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-3 max-w-[1300px] mx-auto py-16 ">
         
         @include('include.categoryBox', [
             'text' => 'Disassembly & replacement guides for the original iPhone, iPhone 3GS, iPhone 4….',
@@ -99,29 +90,29 @@
 </section>
 
 <section class="bg-white  ">
-    <h2 class="text-4xl font-bold text-center mt-10">Najnovšie správy</h2>
+    <h2 class="text-4xl font-bold text-center mt-10">Latest News</h2>
     <div class="grid lg:grid-cols-3 md:grid-cols-2 max-w-[1300px] mx-auto p-9">
-
-    @include('include.newsBox', [
-            'text' => 'We specialise in Phone repairs for Apple iPhones, iPad, iPod, Samsung, Galaxy, Sony, HTC, Nexus, Motorola, Blackberry & ...',
-            'image' => asset('img/repairMan.jpg'),
-            'info' => 'admin September 17, 2022'
         
-        ] )
-    
-    @include('include.newsBox', [
-            'text' => 'If you are facing any problem with your Tablets / Ipads, Kindly pls go through the menttioned catagories ...',
-            'image' => asset('img/iphone.jpg'),
-            'info' => 'admin October 10, 2022'
+        @include('include.newsBox', [
+                'text' => 'We specialise in Phone repairs for Apple iPhones, iPad, iPod, Samsung, Galaxy, Sony, HTC, Nexus, Motorola, Blackberry & ...',
+                'image' => asset('img/repairMan.jpg'),
+                'info' => 'admin September 17, 2022'
             
-        ] )
+            ] )
+        
+        @include('include.newsBox', [
+                'text' => 'If you are facing any problem with your Tablets / Ipads, Kindly pls go through the menttioned catagories ...',
+                'image' => asset('img/iphone.jpg'),
+                'info' => 'admin October 10, 2022'
+                
+            ] )
 
-    @include('include.newsBox', [
-            'text' => 'We specialist in providing On-Site Computer Desktop Repair Service and Network Support for all sized business, On-Site Computer.',
-            'image' => asset('img/console.jpg'),
-            'info' => 'admin September 29, 2022'
-           
-        ] )
+        @include('include.newsBox', [
+                'text' => 'We specialist in providing On-Site Computer Desktop Repair Service and Network Support for all sized business, On-Site Computer.',
+                'image' => asset('img/console.jpg'),
+                'info' => 'admin September 29, 2022'
+            
+            ] )
     </div>
 
     
